33054eee6e0898ad38fb3a6ae358687b97a2db78
kelly-madlab <kelly@madlab.design>
2020-03-01 13:23:55 +0200

    updated api

--- src/components/QuotesList.js
+++ src/components/QuotesList.js
@@ -4,36 +4,40 @@ import '../../src/App.css';
 
 
 export default class QuotesList extends Component {
-    
+
     constructor(props) {
         super(props);
-        this.state = {quotes: []};
-        this.handleClick=this.handleClick.bind(this);
+        this.state = { quotes: [] };
+        this.handleClick = this.handleClick.bind(this);
     }
 
-     componentDidMount(){
+    componentDidMount() {
         if (this.state.quotes.length === 0) this.getQuotes();
     }
 
-async getQuotes(){
-        let quotes = [];
-            let res = await axios.get("http://quotes.stormconsultancy.co.uk/random.json", {
-                headers: {Accept: "application/json"}
-            });
-            quotes.push(res.data.quote);
-        this.setState({quotes: quotes});
+    async getQuotes() {
+        let res = await axios.get("https://quotes.rest/qod?language=en", {
+            headers: { Accept: "application/json" }
+        });
+
+        let quotes = res.data.contents.quotes[0];
+        console.log(quotes);
+        this.setState({ quotes: quotes });
     }
 
-handleClick () {
-    this.getQuotes();
-}
+    handleClick() {
+        this.getQuotes();
+    }
 
     render() {
         return (
             <div className="QuotesList-Wrapper">
-                <h1 className="QuotesList-Header">Quotes for Developers</h1>
+                <h1 className="QuotesList-Header">Quote of the Day...</h1>
                 <div className="QuotesList-Quotes">
-                    {this.state.quotes}
+                    {this.state.quotes.quote}
+                </div>
+                <div className="QuotesList-Author">
+                    {this.state.quotes.author}
                 </div>
                 <button className="QuotesList-Button" onClick={this.handleClick}>New Quote!</button>
             </div>
